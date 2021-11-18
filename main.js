9d3625d158b0a56262c2f3c1faa16991f67697b9
Duck <duckradiation@gmail.com>
2021-11-18 20:49:03 +0000

    Stuff

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
-const {spawn} = require(`child_process`);
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const path = require(`path`);
 
-const child = spawn(`typeperf`, [`-cf`, `counters.txt`], {shell: false});
-child.stdout.on(`data`, (rawData) => {
-  console.log(rawData.toString());
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
+
+io.on("connection", (socket) => {
+  let id = socket.id
+  console.log(`Socket: ${id} connected`);
+
+  socket.on(`disconnect`, (socket) => {
+    console.log(`Socket: ${id} disconnected`)
+  })
+
+  socket.on(`connect_error`, (socket) => {
+    console.log(`Socket: ${id} connect error`)
+  })
+});
+
+app.use(express.static(path.join(__dirname, `public`)))
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, `public/index.html`))
 })
+
+httpServer.listen(3000);
