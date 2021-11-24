09854d3cc4def92bf7751bf65a358804a49bf479
Duck <duckradiation@gmail.com>
2021-11-24 22:08:22 +0000

    Stuff

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,6 +2,9 @@ const socket = io();
 socket.on(`connect`, () => {
   console.log(socket.id);
 })
+socket.on(`dataPacket`, (data) => {
+  console.log(data);
+})
 window.addEventListener(`load`, () => {
 
 })
